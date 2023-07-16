3cd2c57795dae1d3381da24105b0019c5be093f1
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-16 17:53:55 -0300

    remover imports não utilizados

--- src/main/java/br/com/banco/model/entities/Conta.java
+++ src/main/java/br/com/banco/model/entities/Conta.java
@@ -1,6 +1,5 @@
 package br.com.banco.model.entities;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -11,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
