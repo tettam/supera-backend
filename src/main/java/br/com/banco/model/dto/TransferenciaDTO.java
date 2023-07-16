7aa0bab2e440a90d4513261b43562ec83984cb4b
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-16 19:24:29 -0300

    criar dto da entidade Transferencia

---
+++ src/main/java/br/com/banco/model/dto/TransferenciaDTO.java
@@ -0,0 +1,31 @@
+package br.com.banco.model.dto;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import org.springframework.beans.BeanUtils;
+
+import br.com.banco.model.entities.Transferencia;
+import lombok.Data;
+
+@Data
+public class TransferenciaDTO {
+
+  private ZonedDateTime dataTransferencia;
+  private BigDecimal valor;
+  private String tipo;
+  private String nomeOperadorTransicao;
+
+  public TransferenciaDTO(Transferencia transferencia) {
+    this.dataTransferencia = transferencia.getDataTransferencia();
+    this.valor = transferencia.getValor();
+    this.tipo = transferencia.getTipo();
+    this.nomeOperadorTransicao = transferencia.getNomeOperadorTransicao();
+  }
+
+  public Transferencia converterTransferencia(TransferenciaDTO transferenciaDTO) {
+    Transferencia transferencia = new Transferencia();
+    BeanUtils.copyProperties(transferenciaDTO, transferencia);
+    return transferencia;
+  }
+}
