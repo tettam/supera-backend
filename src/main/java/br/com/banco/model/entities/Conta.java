544fe1a65c67947e0962191a296f361bca4b5e31
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-16 13:42:01 -0300

    criar entidade transferencia e associar a entidade conta

--- src/main/java/br/com/banco/model/entities/Conta.java
+++ src/main/java/br/com/banco/model/entities/Conta.java
@@ -1,10 +1,16 @@
 package br.com.banco.model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 
 import lombok.Data;
 
@@ -19,4 +25,7 @@ public class Conta {
 
   @Column(name = "nomeResponsavel")
   private String nome;
+
+  @OneToMany(mappedBy = "conta", cascade = CascadeType.ALL)
+  List<Transferencia> transferencias;
 }
---
+++ src/main/java/br/com/banco/model/entities/Transferencia.java
@@ -0,0 +1,31 @@
+package br.com.banco.model.entities;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Transferencia {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private ZonedDateTime dataTransferencia;
+  private BigDecimal valor;
+  private String tipo;
+  private String nomeOperadorTransicao;
+
+  @ManyToOne
+  @JoinColumn(name = "conta_id")
+  private Conta conta;
+}
