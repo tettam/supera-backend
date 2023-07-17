283de0344208d9e7576689076f5ae723cb03f8b9
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-16 22:47:14 -0300

    adicionar endpoint para buscar filtros

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -1,12 +1,15 @@
 package br.com.banco.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.model.dto.TransferenciaDTO;
@@ -20,7 +23,12 @@ public class TransferenciaController {
   private TransferenciaService transferenciaService;
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<List<TransferenciaDTO>> findAll(@PathVariable Long id){
+  public ResponseEntity<List<TransferenciaDTO>> getTransferencias(
+    @PathVariable Long id,
+    @RequestParam(required = false) String nomeOperador,
+    @RequestParam(required = false) LocalDateTime dataInicio,
+    @RequestParam(required = false) LocalDateTime dataFinal){
+
     List<TransferenciaDTO> transferenciasDTO = transferenciaService.findAll(id);
     return ResponseEntity.ok().body(transferenciasDTO);
   }
