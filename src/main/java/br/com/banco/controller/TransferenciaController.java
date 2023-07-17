9747d246ea8f6480a003b6f24fe3e22b5f228a81
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-17 16:51:58 -0300

    configurar CrossOrigin

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class TransferenciaController {
   private TransferenciaService transferenciaService;
 
   @GetMapping(value = "/{id}")
+  @CrossOrigin
   public ResponseEntity<List<TransferenciaDTO>> getTransferencias(
     @PathVariable Long id,
     @RequestParam(required = false) String nomeOperadorTransicao,
