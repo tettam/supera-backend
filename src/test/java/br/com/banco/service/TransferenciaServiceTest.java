6761d3547c47ebf78b5730d95aa9c7e19622bca1
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-07-18 12:12:35 -0300

    teste soma valores

--- src/test/java/br/com/banco/service/TransferenciaServiceTest.java
+++ src/test/java/br/com/banco/service/TransferenciaServiceTest.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +18,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.banco.model.dto.TransferenciaDTO;
 import br.com.banco.model.entities.Conta;
 import br.com.banco.model.entities.Transferencia;
 import br.com.banco.repository.ContaRepository;
@@ -39,7 +44,7 @@ public class TransferenciaServiceTest {
   @BeforeEach
   public void setUp(){
     Long id = 1L;
-    Conta conta = new Conta();
+    conta = new Conta();
     conta.setId(id);
   }
 
@@ -59,5 +64,32 @@ public class TransferenciaServiceTest {
       transferenciaService.findConta(idInexistente);
     });
   }
+
+  // @Test
+  // public void testValorFiltradoTransferencias(){
+  //   List<TransferenciaDTO> transferencias = new ArrayList<>();
+  //   TransferenciaDTO transferenciaDTO1 = new TransferenciaDTO();
+  //   TransferenciaDTO transferenciaDTO2 = new TransferenciaDTO();
+  //   ZonedDateTime zonedDateTime = ZonedDateTime.parse("2019-01-01T12:00:00");
+  //   Transferencia transferencia1 = new Transferencia();
+  //   Transferencia transferencia2 = new Transferencia();
+
+  //   transferenciaDTO1.setId(1L);
+  //   transferenciaDTO1.setNomeOperadorTransicao("Joao");
+  //   transferenciaDTO1.setValor(new BigDecimal("-500.00"));
+  //   transferenciaDTO1.setTipo("SAQUE");
+  //   transferenciaDTO1.setDataTransferencia(zonedDateTime);
+
+  //   transferenciaDTO2.setId(2L);
+  //   transferenciaDTO2.setNomeOperadorTransicao("José");
+  //   transferenciaDTO2.setValor(new BigDecimal("1000.00"));
+  //   transferenciaDTO2.setTipo("TRANSFERENCIA");
+  //   transferenciaDTO2.setDataTransferencia(zonedDateTime);
+
+  //   transferencias.add(transferencia1);
+  //   transferencias.add(transferencia2);
+  //   BigDecimal valorTotal = testValorFiltradoTransferencias(transferencias);
+  //   assertEquals(new BigDecimal("500"), valorTotal);
+  // }
    
 }
